e1ea2147ebb616e3cc547dbf105bd2da471516ca
Daniel Dehne <danny.dehne@gmail.com>
2023-11-08 09:13:44 +0100

    set neovide options only in neovide

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,5 +2,7 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
--- neovide guifont
-vim.opt.guifont = { "monospace", ":h10" }
+if vim.g.neovide then
+  -- neovide guifont
+  vim.opt.guifont = { "monospace", ":h10" }
+end
